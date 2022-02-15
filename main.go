e165043d3dee7d993379f0215e6cc60824c04f59
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2022-02-15 11:24:15 +0000

    Tidy up codebase
    Split up code into clearer functions, bulk out localstack initialization, add tests
    
    Fixes VEGA-1168 #minor

--- main.go
+++ main.go
@@ -2,115 +2,154 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
-	"os/exec"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 type EventRecord struct {
 	S3 struct {
 		Bucket struct {
-			Name string `json:name`
-		} `json:bucket`
+			Name string `json:"name"`
+		} `json:"bucket"`
 		Object struct {
-			Key string `json:key`
-		} `json:object`
-	} `json:s3`
+			Key string `json:"key"`
+		} `json:"object"`
+	} `json:"s3"`
 }
 
 type ObjectCreatedEvent struct {
-	Records []EventRecord `json:Records`
+	Records []EventRecord `json:"Records"`
 }
 
 type MyResponse struct {
 	Message string `json:"message"`
 }
 
-func HandleLambdaEvent(event ObjectCreatedEvent) (MyResponse, error) {
-  bucketName := event.Records[0].S3.Bucket.Name
-  objectKey := event.Records[0].S3.Object.Key
+type LambdaTagValues struct {
+	pass string
+	fail string
+}
 
-	sess := session.Must(session.NewSession())
+type Downloader interface {
+	Download(w io.WriterAt, input *s3.GetObjectInput, options ...func(*s3manager.Downloader)) (n int64, err error)
+}
 
-	endpoint := os.Getenv("AWS_S3_ENDPOINT")
-	sess.Config.Endpoint = &endpoint
-	sess.Config.S3ForcePathStyle = aws.Bool(true)
+type Scanner interface {
+	ScanFile(path string) (bool, error)
+}
 
-	downloader := s3manager.NewDownloader(sess)
+const tmpFilePath = "/tmp/file"
 
-	filename := "/tmp/file"
-	f, err := os.Create(filename)
+type Lambda struct {
+	tagKey     string
+	tagValues  LambdaTagValues
+	scanner    Scanner
+	s3         s3iface.S3API
+	downloader Downloader
+}
+
+func (l *Lambda) downloadFile(bucket string, key string) error {
+	f, err := os.Create(tmpFilePath)
 	if err != nil {
-		log.Printf("failed to create file %q, %v", filename, err)
-		return MyResponse{}, fmt.Errorf("failed to create file %q, %v", filename, err)
+		return fmt.Errorf("failed to create file %q, %w", tmpFilePath, err)
 	}
 
-	_, err = downloader.Download(f, &s3.GetObjectInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String(objectKey),
+	_, err = l.downloader.Download(f, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
 	})
 
 	if err != nil {
-		log.Printf("failed to download file, %v", err)
-		return MyResponse{}, fmt.Errorf("failed to download file, %v", err)
+		return fmt.Errorf("failed to download file, %w", err)
 	}
 
-	// scan with clamav
-	status := "ok"
-	cmd := exec.Command("./bin/clamscan", filename)
-	if err := cmd.Run(); err != nil {
-		if exitError, ok := err.(*exec.ExitError); ok {
-			if exitError.ExitCode() == 1 {
-				status = "infected"
-			} else {
-				log.Printf("failed to scan file, %v", err)
-				return MyResponse{}, fmt.Errorf("failed to scan file, %v", err)
-			}
-		} else {
-			log.Printf("failed to scan file, %v", err)
-			return MyResponse{}, fmt.Errorf("failed to scan file, %v", err)
-		}
-	}
+	return nil
+}
 
-	// tag document
-	s3client := s3.New(sess)
-
-	tagging, err := s3client.GetObjectTagging(&s3.GetObjectTaggingInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String(objectKey),
+func (l *Lambda) tagFile(bucket string, key string, status string) error {
+	tagging, err := l.s3.GetObjectTagging(&s3.GetObjectTaggingInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
 	})
 
 	if err != nil {
-		log.Printf("failed to get tags, %v", err)
-		return MyResponse{}, fmt.Errorf("failed to get tags, %v", err)
+		return fmt.Errorf("failed to get tags, %w", err)
 	}
 
-	_, err = s3client.PutObjectTagging(&s3.PutObjectTaggingInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String(objectKey),
+	_, err = l.s3.PutObjectTagging(&s3.PutObjectTaggingInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
 		Tagging: &s3.Tagging{
 			TagSet: append(tagging.TagSet, &s3.Tag{
-				Key:   aws.String("virus-scan-status"),
+				Key:   aws.String(l.tagKey),
 				Value: aws.String(status),
 			}),
 		},
 	})
 
 	if err != nil {
-		log.Printf("failed to write tags, %v", err)
-		return MyResponse{}, fmt.Errorf("failed to write tags, %v", err)
+		return fmt.Errorf("failed to write tags, %w", err)
 	}
 
-	log.Printf("scanning complete, tagged with %s", status)
-	return MyResponse{Message: fmt.Sprintf("scanning complete, tagged with %s", status)}, nil
+	return nil
+}
+
+func (l *Lambda) HandleEvent(event ObjectCreatedEvent) (MyResponse, error) {
+	bucketName := event.Records[0].S3.Bucket.Name
+	objectKey := event.Records[0].S3.Object.Key
+
+	err := l.downloadFile(bucketName, objectKey)
+	if err != nil {
+		log.Print(err)
+		return MyResponse{}, err
+	}
+
+	status, err := l.scanner.ScanFile(tmpFilePath)
+	if err != nil {
+		log.Print(err)
+		return MyResponse{}, err
+	}
+
+	statusString := l.tagValues.fail
+	if status {
+		statusString = l.tagValues.pass
+	}
+
+	err = l.tagFile(bucketName, objectKey, statusString)
+	if err != nil {
+		log.Print(err)
+		return MyResponse{}, err
+	}
+
+	log.Printf("scanning complete, tagged with %s", statusString)
+	return MyResponse{Message: fmt.Sprintf("scanning complete, tagged with %s", statusString)}, nil
 }
 
 func main() {
-	lambda.Start(HandleLambdaEvent)
+	sess := session.Must(session.NewSession())
+
+	endpoint := os.Getenv("AWS_S3_ENDPOINT")
+	sess.Config.Endpoint = &endpoint
+	sess.Config.S3ForcePathStyle = aws.Bool(true)
+
+	l := &Lambda{
+		tagKey: os.Getenv("ANTIVIRUS_TAG_KEY"),
+		tagValues: LambdaTagValues{
+			pass: os.Getenv("ANTIVIRUS_TAG_VALUE_PASS"),
+			fail: os.Getenv("ANTIVIRUS_TAG_VALUE_FAIL"),
+		},
+		scanner:    &ClamAvScanner{},
+		s3:         s3.New(sess),
+		downloader: s3manager.NewDownloader(sess),
+	}
+
+	lambda.Start(l.HandleEvent)
 }
