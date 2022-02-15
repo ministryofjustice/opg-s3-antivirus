3a2db9b75eeb1ba36aa558811dc075a98fb6f7e7
Greg Tyler <gregtyler@users.noreply.github.com>
2022-02-15 12:51:48 +0000

    Tidy up codebase (#2)
    Split up code into clearer functions, bulk out localstack initialization, add tests
    
    Fixes VEGA-1168 #minor

---
+++ scripts/localstack/wait/wait-until-tagged.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+set -uo pipefail
+
+echo "Waiting for \"$1\" to be tagged"
+
+iterations=0
+
+buckets=""
+
+while [[ "$iterations" -lt 60 ]]
+do
+  tags=$(awslocal s3api get-object-tagging --bucket uploads-bucket --key $1 2> /dev/null) || true
+
+  if [[ $tags = *virus-scan-status* ]]
+  then
+    exit 0
+  fi
+
+  ((iterations++))
+  printf '.'
+  sleep 1
+done
+
+echo "Waited $iterations seconds for the file before giving up"
+echo "tag results:"
+echo "----------------------------------"
+echo "$tags"
+echo "----------------------------------"
+
+exit 1
