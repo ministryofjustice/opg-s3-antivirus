592ac98cca88ae3957bbf82d6526cc96af9693c3
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-05-01 23:50:02 +0100

    switch to alpine install zip create myFunction.zip

--- scripts/build-zip/build-zip.sh
+++ scripts/build-zip/build-zip.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-set -e
+apk add zip
 
 go mod download
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -tags lambda.norpc -o bootstrap ./opg-s3-antivirus
+
+zip myFunction.zip ./bootstrap
