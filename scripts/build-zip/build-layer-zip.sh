ad53695a12ca69de9b497a89d74e370b42eb82e0
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-05-07 17:45:22 +0100

    temporarily unzip into usr (clamav default dir)

--- scripts/build-zip/build-layer-zip.sh
+++ scripts/build-zip/build-layer-zip.sh
@@ -13,13 +13,16 @@ mkdir -p /tmp/build
 pushd /tmp/build
 
 printf "Download the clamav and clamd package\n\n"
-dnf download --downloaddir=/tmp/build --resolve clamav-${VERSION} clamd-${VERSION}
+dnf download --downloaddir=/tmp/build --resolve clamav-"${VERSION}" clamd-"${VERSION}"
 
 printf "Convert clamav and clamd RPM to CPIO stream (-vmid verbose, preserve-modification-time, extract, make-directories)\n\n"
 rpm2cpio clamav*.rpm | cpio -vimd
 rpm2cpio clamd*.rpm | cpio -vimd
+rpm2cpio clamav-lib-*.rpm | cpio -vimd
 
-# echo "!!!>>>>>Download other package dependencies"
+
+
+# printf "!!!>>>>>Download other package dependencies\n\n"
 # rpm2cpio libargon2-*.rpm | cpio -vimd
 # rpm2cpio gzip-*.rpm     | cpio -vimd
 # rpm2cpio pam-*.rpm     | cpio -vimd
@@ -51,17 +54,19 @@ rpm2cpio clamd*.rpm | cpio -vimd
 # rpm2cpio libeconf-*.rpm | cpio -vimd
 
 # list what we're moving
-tree /tmp/build/usr/
-tree /opt/app
+# tree /tmp/build/usr/
+# tree /opt/app
 
 printf "move binaries and lib for zipping\n\n"
-mkdir -p bin lib etc
+mkdir -p bin lib etc lib64
 
 cp /tmp/build/usr/bin/clamdscan bin/.
 cp /tmp/build/usr/sbin/clamd bin/.
 cp -R /tmp/build/usr/lib/* lib/.
+cp -R /tmp/build/usr/lib64/* lib64/.
 cp /app/clamd.conf etc/.
 
 zip -r9 /app/lambda_layer.zip bin
 zip -r9 /app/lambda_layer.zip lib
+zip -r9 /app/lambda_layer.zip lib64
 zip -r9 /app/lambda_layer.zip etc
