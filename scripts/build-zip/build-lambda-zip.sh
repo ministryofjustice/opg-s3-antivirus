a4624350ccdec9c43cf6c656f39c80989c735f24
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-05-09 13:33:30 +0100

    fix zipping bootstrap

--- scripts/build-zip/build-lambda-zip.sh
+++ scripts/build-zip/build-lambda-zip.sh
@@ -6,4 +6,4 @@ go mod download
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -tags lambda.norpc -o bootstrap ./opg-s3-antivirus
 
-zip myFunction.zip ./bootstrap
+zip myFunction.zip bootstrap
