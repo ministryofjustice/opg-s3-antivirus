4e9faa4792a449fe0f3ea608631216b54a84648f
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-05-10 13:37:18 +0100

    cleanup test

--- scripts/build-zip/test-layer-zip.sh
+++ scripts/build-zip/test-layer-zip.sh
@@ -11,9 +11,5 @@ unzip lambda_layer.zip -d /opt
 export PATH=/opt/bin:$PATH
 export LD_LIBRARY_PATH=/opt/lib:$LD_LIBRARY_PATH
 
-ls -al /opt
-
-cat /opt/etc/clamd.conf
-
 clamd --config-file /opt/etc/clamd.conf --version
 clamdscan --config-file /opt/etc/clamd.conf --version
